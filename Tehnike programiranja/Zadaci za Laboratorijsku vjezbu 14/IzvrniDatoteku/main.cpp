a9ad5715f42429fb2ddc979636fee2a5dd935303
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-10-27 19:45:02 +0200

    a

--- Tehnike programiranja/Zadaci za Laboratorijsku vjezbu 14/IzvrniDatoteku/main.cpp
+++ Tehnike programiranja/Zadaci za Laboratorijsku vjezbu 14/IzvrniDatoteku/main.cpp
@@ -6,12 +6,18 @@ using namespace std;
 
 int main()
 {
-    vector<int> vektorA ;
-        ifstream file("test.bin", ios::binary);
-    for(auto a : vektorA){
-         file.read((char *)&a, sizeof(a));
+    double niz[5] = {1,2,3,4,5};
+    ofstream novifajl("data.dat", ios::out | ios::binary);
+    for (auto&& i : niz)
+        novifajl << i;
+    novifajl.close();
+    ifstream citaj("data.dat", ios::in | ios::binary);
+    citaj.seekg(-1, ios::end);
+    char ch;
+    for(; citaj.get(ch); citaj.seekg(-2, ios::cur))
+    {
+        cout << ch;
     }
-    file.close();
 
     return 0;
 }
