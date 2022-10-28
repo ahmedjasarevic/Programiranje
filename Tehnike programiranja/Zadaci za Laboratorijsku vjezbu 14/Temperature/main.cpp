44516f2125549890e9ef6d73d6185f58a2003679
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-10-28 19:49:41 +0200

    up

--- Tehnike programiranja/Zadaci za Laboratorijsku vjezbu 14/Temperature/main.cpp
+++ Tehnike programiranja/Zadaci za Laboratorijsku vjezbu 14/Temperature/main.cpp
@@ -10,25 +10,18 @@ using namespace std;
 
 int main()
 {
-    string s;
     ifstream novifajl("TEMPERATURE.txt");
-
     if(novifajl.fail())
     {
         cerr << "Gteska" << endl;
     }
     string linija;
-    int tempBroj,brojac = 0,suma = 0;
-    double prosjecnaTemp;
+    int brojac = 0;
     char znak;
-    int minTemp = INT_MAX;
-    int maxTemp = INT_MIN;
-    vector<string> datumi;
-    vector<string> nazivi;
-    vector<string> stringBrojevi;
-    vector<int> tempere;
-    int brojacLinija = 0,brojacProsjeka = 0;
-    int broj = 0;
+    vector<string> datumi,nazivi,stringBrojevi;
+    vector<double> sumaVec;
+    vector<int> maxVec,minVec;
+    int brojacLinija = -1,broj;
     while(getline(novifajl,linija))
     {
         if(brojac == 1)
@@ -43,39 +36,47 @@ int main()
         }
         else if(brojac == 2)
         {
-          stringBrojevi.push_back(linija);
+            stringBrojevi.push_back(linija);
             brojac = 0;
         }
         brojacLinija++;
     }
 
-    for(auto a :stringBrojevi){
-            stringstream ss(a);
-    while (ss >> broj)
+    for(auto a :stringBrojevi)
     {
-        tempere.push_back(broj);
+        stringstream ss(a);
+        double suma = 0,brojac = 0;
+        int min = INT_MAX;
+        int max = INT_MIN;
+        while (ss >> broj)
+        {
+            suma += broj;
+            brojac++;
+            if(broj < min)
+            {
+                min = broj;
+            }
+            if(broj > max)
+            {
+                max = broj;
+            }
+            if (ss.peek() == ',') ss.ignore();
+        }
+        sumaVec.push_back(double(suma)/brojac);
+        minVec.push_back(min);
+        maxVec.push_back(max);
 
-        if (ss.peek() == ',')
-            ss.ignore();
     }
-    ss.clear();
-
-
-    }
-    for(auto a : tempere){
-        cout <<a;
-    }
-
-
-
-
     fstream drugifajl("IZVJESTAJ.txt", ios::out);
-    for(auto i = 0; i < brojacLinija-1; i++)
+    for(auto i = 0; i < brojacLinija; i++)
     {
+        if(nazivi[i] == "") break;
         drugifajl << nazivi[i] << endl;
         drugifajl << "--------------------" <<endl;
         drugifajl << "Datum mjerenja : " << datumi[i] << endl;
-        drugifajl << "Temperature mjerenja : "  << endl;
+        drugifajl << "Minimalna temperatura: " << minVec[i] << endl;
+        drugifajl << "Maksimalna temperatura: " << maxVec[i] << endl;
+        drugifajl << "Prosjecna temperatura : "  <<  sumaVec[i] << endl;
         drugifajl << endl;
 
     }
